d1b4761ae00b4e74a2fd58e682b7534e53b91732
toprisela13 <tomislav.priselac@farmeron.com>
2017-05-09 19:21:49 +0200

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
-//const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
-//const extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: 'source-map',
 	entry: path.resolve(__dirname, 'src', 'js', 'Main.js'),
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: '[hash].js'
 	},
 	module: {
 		rules: [
@@ -21,7 +19,6 @@ module.exports = {
 				test: /\.scss$/,
 				include: path.resolve(__dirname, 'src', 'css'),
 				exclude: /node_modules/,
-				//use: extractTextPlugin.extract({ fallback: "style-loader", use: ["css-loader", "sass-loader"] }),
 				use: [{
 					loader: "style-loader" // creates style nodes from JS strings
 				}, {
@@ -42,6 +39,5 @@ module.exports = {
 		new htmlWebpackPlugin({
 			template: 'index.html',
 		}),
-		//new extractTextPlugin(path.join(__dirname, 'dist', 'bundle.css')),
 	]
 }
